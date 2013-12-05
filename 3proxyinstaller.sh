a0b9834de431f9459ac03d604869ddef5f3b0a30
benjamin74 <benjamin.aguettaz@gmail.com>
2013-12-05 16:53:50 +0100

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,5 +1,5 @@
 apt-get update && apt-get -y upgrade
-apt-get install -y build-essential
+apt-get install -y build-essential nano
 wget http://3proxy.ru/0.6.1/3proxy-0.6.1.tgz
 tar xzf 3proxy-0.6.1.tgz
 cd 3proxy-0.6.1
@@ -14,4 +14,3 @@ cd /etc/init.d/
 wget --no-check-certificate https://raw.github.com/benjamin74/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
-/etc/3proxy/3proxy /etc/3proxy/3proxy.cfg &
