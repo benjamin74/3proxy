7ab47719cd7893e077efae0e73b4b8183912af0a
benjamin74 <benjamin.aguettaz@gmail.com>
2013-12-05 16:25:39 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,2 +1,11 @@
 apt-get update && apt-get -y upgrade
 apt-get install -y build-essential
+wget http://3proxy.ru/0.6.1/3proxy-0.6.1.tgz
+tar xzf 3proxy-0.6.1.tgz
+cd 3proxy-0.6.1
+make -f Makefile.Linux
+cd src
+mkdir /etc/3proxy/
+mv 3proxy /etc/3proxy/
+cd /etc/3proxy/
+wget 
