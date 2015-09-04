0671a7fdc3cb386192afd016d014f5257ea5dd45
Baran Kılıç <barankilic@gmail.com>
2015-09-04 23:35:58 +0300

    Update 3proxyinstaller.sh

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -1,6 +1,6 @@
 apt-get update && apt-get -y upgrade
 apt-get install -y build-essential nano
-wget http://3proxy.ru/0.6.1/3proxy-0.6.1.tgz
+wget --no-check-certificate https://github.com/z3APA3A/3proxy/archive/v0.7.1.2.tar.gz
 tar xzf 3proxy-0.6.1.tgz
 cd 3proxy-0.6.1
 make -f Makefile.Linux
@@ -8,9 +8,9 @@ cd src
 mkdir /etc/3proxy/
 mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
-wget --no-check-certificate https://github.com/benjamin74/3proxy/raw/master/3proxy.cfg
+wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
 cd /etc/init.d/
-wget --no-check-certificate https://raw.github.com/benjamin74/3proxy/master/3proxyinit
+wget --no-check-certificate https://raw.github.com/barankilic/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
 update-rc.d 3proxyinit defaults
