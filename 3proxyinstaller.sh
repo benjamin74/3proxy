328e5b6f5f2be35712bc8647903125a2a7b76dfd
Baran Kılıç <barankilic@gmail.com>
2015-11-13 15:29:37 +0200

    Added ready to use .proxyauth file example

--- 3proxyinstaller.sh
+++ 3proxyinstaller.sh
@@ -10,6 +10,8 @@ mv 3proxy /etc/3proxy/
 cd /etc/3proxy/
 wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/3proxy.cfg
 chmod 600 /etc/3proxy/3proxy.cfg
+wget --no-check-certificate https://github.com/barankilic/3proxy/raw/master/.proxyauth
+chmod 600 /etc/3proxy/.proxyauth
 cd /etc/init.d/
 wget --no-check-certificate https://raw.github.com/barankilic/3proxy/master/3proxyinit
 chmod  +x /etc/init.d/3proxyinit
