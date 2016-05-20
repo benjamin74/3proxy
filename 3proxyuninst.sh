dc0dfaaf81669afb3ebcb230e572bbb38377c415
Baran Kılıç <barankilic@gmail.com>
2016-05-20 15:10:52 +0300

    3Proxy uninstaller

---
+++ 3proxyuninst.sh
@@ -0,0 +1,10 @@
+/etc/init.d/3proxyinit stop
+rm -rf /etc/3proxy
+rm /etc/rc0.d/K203proxyinit
+rm /etc/rc1.d/K203proxyinit 
+rm /etc/rc6.d/K203proxyinit
+rm /etc/rc2.d/S203proxyinit
+rm /etc/rc3.d/S203proxyinit 
+rm /etc/rc4.d/S203proxyinit
+rm /etc/rc5.d/S203proxyinit
+rm /etc/init.d/3proxyinit
